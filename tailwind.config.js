7d971c5826bfedc964bdd7d57d2ccdbdfba78a7c
unknown <nikhil18981@gmail.com>
2025-07-02 01:41:11 +0530

    upto testimonial section

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        charcoal: "#121212",
+        gold: "#D4AF37",
+      },
+      fontFamily: {
+        serif: ["'Playfair Display'", "serif"],
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
